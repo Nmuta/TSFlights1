5ea44322a21700f2a96ca344fdc1cdb27b63f42c
Nmuta Jones <nmuta.jones@charter.com>
2020-03-01 20:00:01 -0700

    Reformat Dates

--- src/app/flights.service.ts
+++ src/app/flights.service.ts
@@ -8,11 +8,13 @@ export class FlightsService {
 
   flights: Flight[] = [
      {origin: "Miami", destination: 'Chicago', flightNumber: 345,
-     depart: '2020-02-25T23:18:21.932Z' as unknown as Date,
-     arrive: '2020-02-25T23:21:21.932Z' as unknown as Date, nonstop: true}, 
+     depart: new Date("2020-02-25T23:18:21.932Z"),
+     arrive: new Date('2020-02-25T23:21:21.932Z'), 
+     nonstop: true}, 
      {origin: "New York", destination: 'Los Angeles', flightNumber: 432,
-     depart: '2020-05-25T23:18:00.932Z' as unknown as Date,  
-     arrive: '2020-05-25T23:23:21.932Z' as unknown as Date, nonstop: false}, 
+     depart: new Date('2020-05-25T23:18:00.932Z'),  
+     arrive: new Date('2020-05-25T23:23:21.932Z'), 
+     nonstop: false}, 
      ];
 
   constructor() { }
